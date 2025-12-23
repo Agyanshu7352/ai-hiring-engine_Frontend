c54e58ea18b1c7fb55f86ca00a36bd6b935d9a85
Agyanshu Kumar <agyanshukumar@gmail.com>
2025-12-23 19:30:24 +0530

    fixing import files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { NAV_ITEMS } from '../config/navConfig';
+import { NAV_ITEMS } from '../config/navconfig';
 
 const Navbar = () => {
   const location = useLocation();
