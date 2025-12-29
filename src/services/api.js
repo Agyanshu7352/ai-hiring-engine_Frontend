b8ffdc6ac436ad7568acc7a39221ee6b16ccf037
Agyanshu Kumar <agyanshukumar@gmail.com>
2025-12-29 12:11:29 +0530

    link correction

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api' || process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = 'http://localhost:5000/api' || process.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
